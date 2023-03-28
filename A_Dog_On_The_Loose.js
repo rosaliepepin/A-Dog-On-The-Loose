5321bfdcfc1b742758440b5531effef66952ec91
rosaliepepin <129199485+rosaliepepin@users.noreply.github.com>
2023-03-28 12:50:34 -0400

    Update A_Dog_On_The_Loose.js

--- A_Dog_On_The_Loose.js
+++ A_Dog_On_The_Loose.js
@@ -5,11 +5,15 @@
 let img1, img2, img // declare two image variables
 let textX = -100
 let textY = -100
+let positionCalm = 500
+let positionWave = -500
 
 function preload() {
   img1 = loadImage("waveL.png")
   img2 = loadImage("waveR.png")
-  img = loadImage("calm.png")
+	imgCalm = loadImage("calm.png")
+	font = loadFont("dogfont.ttf")
+	
 
 }
 
@@ -17,18 +21,21 @@ function setup() {
   createCanvas(windowWidth, windowHeight)
   sidewalk=loadImage("sidewalk.jpeg")
   img = img1 // set the initial image to img1
-  dog = loadImage("dog.png")
+	dog = loadImage("dog.png")
 }
 
 function draw() {
-  background(sidewalk)
-  image(img,500,250,575,750)
-  image(dog, mouseX, 900, 150, 200)
-  text("bark", textX, textY);
+	background(sidewalk)
+  image(img,positionWave,200,575,750)
+	image(imgCalm,positionCalm,200,575,750)
+	image(dog, mouseX, 850, 150, 200)
+	text("bark", textX, textY);
 
 }
 
 function mouseClicked() {
+	 positionCalm = -500
+	 positionWave = 500
   if (img == img1) { // if the current image is img1, switch to img2
     img = img2
   } else { // otherwise, switch to img1
@@ -39,8 +46,9 @@ function mouseClicked() {
 function keyPressed() {
   if (key === 'b') {
     textSize(45);
-    textX = mouseX + random(-50,50)
-    textY = 880
+		textFont(font)
+		textX = mouseX + random(-50,50)
+		textY = 820
     fill(255,255,255);
   }
 }
